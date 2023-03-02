77067b916042f1b9198499414b4a6cae9e8b5521
SSENONO ROBERT <93611201+Robert-Sayno@users.noreply.github.com>
2023-03-02 10:32:37 +0300

    Add files via upload
    This is an mtn website customised in a webview with kotlin in android studio

---
+++ src/main/java/com/ssenono/mtn/ui/Investors/InvestorsFragment.kt
@@ -0,0 +1,41 @@
+package com.ssenono.mtn.ui.Investors
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.ssenono.mtn.R
+
+class InvestorsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_investors, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myProductView: WebView = view.findViewById(R.id.nav_investors)
+        myProductView.webViewClient = object: WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                url:String
+            ): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+
+        myProductView.loadUrl("https://www.mtn.co.ug/personal/")
+        myProductView.settings.javaScriptEnabled
+        myProductView.settings.allowContentAccess = true
+        myProductView.settings.useWideViewPort = true
+        myProductView.settings.domStorageEnabled = true
+    }
+
+
+}
---
+++ src/main/java/com/ssenono/mtn/ui/home/HomeFragment.kt
@@ -0,0 +1,42 @@
+package com.ssenono.mtn.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.ssenono.mtn.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myProductView: WebView = view.findViewById(R.id.nav_home)
+        myProductView.webViewClient = object: WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                url:String
+            ): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+
+        myProductView.loadUrl("https://www.mtn.co.ug/personal/")
+        myProductView.settings.javaScriptEnabled
+        myProductView.settings.allowContentAccess = true
+        myProductView.settings.useWideViewPort = true
+        myProductView.settings.domStorageEnabled = true
+    }
+
+
+
+}
